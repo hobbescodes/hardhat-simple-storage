1952ae9a676517e2716729b85e0845c3cbc30082
hobbescodes <jakob.hauble@gmail.com>
2022-06-27 19:46:59 -0500

    add programmatic block explorer verification

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,6 @@
 require("@nomiclabs/hardhat-waffle")
 require("dotenv").config()
+require("@nomiclabs/hardhat-etherscan")
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -13,6 +14,7 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 
 const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL
 const PRIVATE_KEY = process.env.PRIVATE_KEY
+const ETHERSCAN_API_KEY = process.ETHERSCAN_API_KEY
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
@@ -29,5 +31,8 @@ module.exports = {
             accounts: [PRIVATE_KEY],
             chainId: 4,
         },
+    },
+    etherscan: {
+        apiKey: ETHERSCAN_API_KEY,
     },
 }
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,6 +1,6 @@
 // if we imported from ethers, ethers doesn't know that we already compiled a SimpleStorage contract
 // but hardhat does
-const { ethers } = require("hardhat")
+const { ethers, run, network } = require("hardhat")
 
 main = async () => {
     const SimpleStorageFactory = await ethers.getContractFactory(
@@ -10,6 +10,26 @@ main = async () => {
     const simpleStorage = await SimpleStorageFactory.deploy()
     await simpleStorage.deployed()
     console.log(`Deployed contract to ${simpleStorage.address}`)
+    if (network.config.chainId === 4 && process.env.ETHERSCAN_API_KEY) {
+        await simpleStorage.deployTransaction.wait(6)
+        await verify(simpleStorage.address, [])
+    }
+}
+
+verify = async (contractAddress, args) => {
+    console.log("Verifying contract...")
+    try {
+        await run("verify:verify", {
+            address: contractAddress,
+            constructorArguments: args,
+        })
+    } catch (e) {
+        if (e.message.toLowerCase().includes("already verified")) {
+            console.log("Already verified!")
+        } else {
+            console.log(e)
+        }
+    }
 }
 
 main()
